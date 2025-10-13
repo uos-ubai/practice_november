1425efef5f3d8942b2864639a3e57c94804f68c3
UBAIUOS <107539532+UBAIUOS@users.noreply.github.com>
2025-10-13 11:36:28 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@
 #SBATCH --output=./output/%j.out
 #SBATCH --error=./output/%j.err
 #SBATCH --partition=gpu1                                # 사용할 파티션 이름
-#SBATCH --nodelist=n002                                 # 사용할 노드 이름
+#SBATCH --nodelist=n107                                 # 사용할 노드 이름
 #SBATCH --gres=gpu:1                                    # 사용할 gpu 수
 #SBATCH --cpus-per-task=1                               # 하나의 태스크가 사용할 CPU 코어 수
-##SBATCH --mem=128G                                     # 메모리 할당량
-##SBATCH --time=48:00:00                                # 최대 실행 시간 (형식: HH:MM:SS)
+##SBATCH --mem=128G                                     # 메모리 할당량 (##이므로 해당 명령어 비활성화)
+##SBATCH --time=48:00:00                                # 최대 실행 시간 (##이므로 해당 명령어 비활성화)
 
 echo "start at: $(date)"                                # 접속한 날짜 표기
 echo "node: $HOSTNAME"                                  # 접속한 노드 번호 표기
@@ -19,8 +19,8 @@ module load cuda/11.8.0
 # Load env (python 환경)
 source ~/ENTER/etc/profile.d/conda.sh     
 
-# 가상환경 활성화
-conda activate ubai                                       # ubai라는 conda 환경에서 슈퍼컴퓨팅 쓸 준비 완료한 것으로, 추후 사용자가 설정한 가상환경 이름으로 변경 필요
+# 가상환경 활성화 (설치한 가상환경 이름으로 변경 필요, ubai -> 가상환경 이름)
+conda activate ubai                                       # ubai라는 conda 환경에서 슈퍼컴퓨팅 쓸 준비 완료
 
 # python 스크립트 실행
-python practice.py
+python test.py
