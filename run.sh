7e02bb035c9fc0ecf6f5b19822d380daf13ce658
ki5n2 <106531096+ki5n2@users.noreply.github.com>
2025-10-24 10:48:04 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=UBAI_JOB                             # 사용자의 작업 이름으로 변경
+#SBATCH --job-name=MY_JOB                               # 사용자의 작업 이름으로 변경
 #SBATCH --output=./output/%j.out
 #SBATCH --error=./output/%j.err
-#SBATCH --partition=gpu1                                # 사용할 파티션 이름
-#SBATCH --nodelist=n107                                 # 사용할 노드 이름
+#SBATCH --partition=gpu_n -> 변경(gpu number)           # 사용할 파티션 이름
+#SBATCH --nodelist=n107   -> 변경(node number)          # 사용할 노드 이름
 #SBATCH --gres=gpu:1                                    # 사용할 gpu 수
 #SBATCH --cpus-per-task=1                               # 하나의 태스크가 사용할 CPU 코어 수
 ##SBATCH --mem=128G                                     # 메모리 할당량 (##이므로 해당 명령어 비활성화)
@@ -17,10 +17,10 @@ echo "jobid: $SLURM_JOB_ID"                             # jobid 표기
 module load cuda/11.8.0 
 
 # Load env (python 환경)
-source ~/ENTER/etc/profile.d/conda.sh     
+source ~/miniconda3/etc/profile.d/conda.sh     
 
-# 가상환경 활성화 (설치한 가상환경 이름으로 변경 필요, ubai -> 가상환경 이름)
-conda activate ubai                                       # ubai라는 conda 환경에서 슈퍼컴퓨팅 쓸 준비 완료
+# 가상환경 활성화 (설치한 가상환경 이름으로 변경 필요, test -> 가상환경 이름)
+conda activate test                                       # test라는 conda 환경에서 슈퍼컴퓨팅 쓸 준비 완료
 
 # python 스크립트 실행
-python test.py
+python predict.py
