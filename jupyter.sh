3b8ebe348bec6bfda5cd7bea15f315539aaa2ad7
ki5n2 <106531096+ki5n2@users.noreply.github.com>
2025-11-03 09:19:40 +0900

    Create jupyter.sh

---
+++ jupyter.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=UBAIJOB                             # 수정필요
+#SBATCH --partition=gpu7                               # 수정필요
+#SBATCH --nodelist=n107                                # 수정필요                
+#SBATCH --cpus-per-task=1                              # 수정필요
+#SBATCH --gres=gpu:1 
+#SBATCH -o ./output/jupyter.%N.%j.out  # STDOUT        # 수정필요
+#SBATCH -e ./output/jupyter.%N.%j.err  # STDERR        # 수정필요
+
+echo "start at:" `date`
+echo "node: $HOSTNAME" 
+echo "jobid: $SLURM_JOB_ID" 
+
+module load cuda/11.8.0 
+
+python -m jupyter lab $HOME \
+        --ip=0.0.0.0 \
+        --no-browser
+
+# 가상환경 내 주피터 설치
+pip install ipykernel jupyterlab
