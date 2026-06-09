cf97abd07401b8b7dfa060403af862d4a9653908
UBAIUOS <107539532+UBAIUOS@users.noreply.github.com>
2026-06-09 14:41:22 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 #SBATCH --job-name=MY_JOB                  # 사용자의 작업 이름으로 변경
-#SBATCH --output=./output/%j.out
-#SBATCH --error=./output/%j.err
-#SBATCH --partition=gpu0                   # 사용할 파티션 이름으로 변경 필요 -> gpu number(gpu1 ~ gpu6)
-#SBATCH --nodelist=n107                    # 사용할 노드 이름으로 변경 -> node number(n001 ~ n106)                  
-#SBATCH --gres=gpu:1                       # 사용할 gpu 수
+#SBATCH --output=/gpfs/home1/[계정명]/practice/output/%j.out
+#SBATCH --output=/gpfs/home1/[계정명]/practice/output/%j.err
+#SBATCH --partition=gpu6                   # 사용할 파티션 이름으로 변경 필요 -> gpu number(gpu1 ~ gpu6)
+#SBATCH --nodes=1                          # 사용할 노드 수 
+#SBATCH --nodelist=n032,n033               # 사용할 노드 이름으로 변경 -> node number(n001 ~ n106)  
 #SBATCH --cpus-per-task=1                  # 하나의 태스크가 사용할 CPU 코어 수
+
+##SBATCH --gres=gpu:1                       # 사용할 gpu 수
 ##SBATCH --mem=128G                        # 메모리 할당량 (##이므로 해당 명령어 비활성화)
 ##SBATCH --time=48:00:00                   # 최대 실행 시간 (##이므로 해당 명령어 비활성화)
 
 echo "start at: $(date)"                   # 접속한 날짜 표기
 echo "node: $HOSTNAME"                     # 접속한 노드 번호 표기
 echo "jobid: $SLURM_JOB_ID"                # jobid 표기
-
-# Load modules (cuda 환경)
-module load cuda/11.8.0 
 
 # Load env (python 환경)
 source ~/miniconda3/etc/profile.d/conda.sh     
